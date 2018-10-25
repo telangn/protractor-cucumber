ed116f7dcd0881d3df4ce2bcf6602d7d8dda44e8
Ninad <telangn@gmail.com>
2018-10-24 20:34:03 -0700

    added getText stepDef

--- features/google.feature
+++ features/google.feature
@@ -2,7 +2,7 @@
 Feature: Assert Google Web Pages
 
   Scenario: Google Home Page
-    Given I am on google page with title 'Google'
+    Given I am on web page with title 'Google'
     When I perform a google search with text 'mail.google.com'
     Then element with locator '#rso > div:nth-child(1) > div > div > div > div > div.r > a > h3' is displayed
 
@@ -11,3 +11,4 @@ Feature: Assert Google Web Pages
     When I scroll to element with locator '.dot.loaded'
     And I click on element with locator '.dot.loaded'
     Then element with locator 'section.scroll-target.flex-container.inbox-tabs-for-gmail.active.activated > div.inbox-tabs-for-gmail__image-box > div' is displayed
+    And element with locator '.inbox-tabs-for-gmail__title' has text value of 'Meet your new inbox'
--- stepDefinitions/steps.js
+++ stepDefinitions/steps.js
@@ -7,7 +7,7 @@ const globals = new Globals();
 const expect = globals.expect;
 
 
-Given('I am on google page with title {string}', function (title) {
+Given('I am on web page with title {string}', function (title) {
     return expect(browser.getTitle()).to.eventually.equal(title);
 });
 
@@ -33,11 +33,16 @@ When('I click on element with locator {string}', function (string) {
 When('I scroll to element with locator {string}', function (string) {
     var element = $(string);
     return browser.actions().mouseMove(element).perform();
-  });
+});
 
 Then('element with locator {string} is displayed', function (locator) {
     var element = $(locator);
     var EC = protractor.ExpectedConditions;
     browser.wait(EC.elementToBeClickable(element), 5000);
     return expect($(locator).isDisplayed()).to.eventually.equal(true);
+});
+
+Then('element with locator {string} has text value of {string}', function (locator, string) {
+    var element = $(locator);
+    return expect(element.getText()).to.eventually.equal(string);
 });
